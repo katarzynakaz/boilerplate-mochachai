86cda07245a134086aca107953eff1b9a85f94c9
Katarzyna K <l453r3@gmail.com>
2024-03-12 13:30:08 +0000

    com

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -34,14 +34,14 @@ suite('Unit Tests', function () {
     // #5
     test('#equal, #notEqual', function () {
       assert.equal(12, '12', 'Numbers are coerced into strings with ==');
-      assert.equal({ value: 1 }, { value: 1 }, '== compares object references');
+      assert.notEqual({ value: 1 }, { value: 1 }, '== compares object references');
       assert.equal(6 * '2', '12');
       assert.notEqual(6 + '2', '12');
     });
     // #6
     test('#strictEqual, #notStrictEqual', function () {
       assert.notStrictEqual(6, '6');
-      assert.notStrictEqual(6, 3 * 2);
+      assert.strictEqual(6, 3 * 2);
       assert.notStrictEqual(6 * '2', 12);
       assert.notStrictEqual([1, 'a', {}], [1, 'a', {}]);
     });
